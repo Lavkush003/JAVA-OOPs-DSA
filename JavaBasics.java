159ba9079a7c2d275a5f1622dd65dd2d36b291c8
Lavkush Nishad <lovekushnishad003@gmail.com>
2026-05-29 17:05:09 +0530

    Java Basics

---
+++ JavaBasics.java
@@ -0,0 +1,98 @@
+// public class JavaBasics{
+//     public static void main(String args[]){
+
+//     System.out.println("Hello World");
+//     }
+// }
+
+//boilerplate
+
+
+
+
+//Variables in Java
+
+// public class JavaBasics{
+//     public static void main(String args[]){
+
+//         int a=10;
+//         int b=5;
+//         System.out.println(a);
+//         System.out.println(b);
+//         String name="John And Jane";
+//         System.out.println(name);
+
+
+ 
+
+//     }
+// }
+
+
+
+//Data types in Java
+
+// public class JavaBasics{
+//     public static void main(String args[]){
+//         byte b=8;
+//         System.out.println(b);
+//         char ch='a';
+//         System.out.println(ch);
+//         boolean var=false;
+//         System.out.println(var);
+//         double price=10.99;
+
+
+//         System.out.println(price);
+//         int number=25;
+
+//         //byte=>1 byte
+//         //short=>2 bytes
+//         //char=>2 bytes
+//         //boolean=>1 bit
+//         //int=>4 bytes
+//         //long=>8 bytes
+//         //float=>4
+//         //double=>8
+
+
+//     }
+// }
+
+
+
+//Input in Java
+
+// import java.util.*;
+// public class JavaBasics{
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         // String input=sc.next();
+//         // System.out.println(input);
+
+//         // String name=sc.nextLine();
+//         // System.out.println(name);
+
+//         // int number=sc.nextInt();
+//         // System.out.println(number);
+
+//         float price=sc.nextFloat();
+//         System.out.println(price);
+
+//     }
+// }
+
+//Type Casting in Java 
+//=> converting one data type to another
+
+// import java.util.*;
+
+// public class JavaBasics{
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         int a=25.12f; //float to int
+//         int b=(int) a;
+//         System.out.println(b);
+
+//     }
+// }
---
+++ Operators.java
@@ -0,0 +1,10 @@
+
+public class Operators{
+    public static void main(String args[]){
+
+        int A=10;
+        int B=5;
+        System.out.println("add= "+(A+B));
+        
+    }
+}
